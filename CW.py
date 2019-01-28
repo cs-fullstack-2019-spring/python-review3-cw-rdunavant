82483b5459fb30262d1dcccda0f23a0e2462e061
rdunavant0 <rdunavant0@gmail.com>
2019-01-28 11:56:04 -0600

    message

---
+++ CW.py
@@ -0,0 +1,45 @@
+#Given number n, return True if n is in range 1..10.
+def problem1():
+    numN = ""
+    return True
+    if numN == range(1..10):
+#Unless outside_mode is True, return True if number is less or equal to 1, or greater or equal to 10.
+if outside_mode = True:
+    return True
+if number <= 1:
+    or
+    number >= 10
+    return True
+    print(result)
+def in1to10(n, outside_mode):
+in1to10(5, False)
+
+#Create function that has loop that quits with equal sign. If user doesn't enter equal sign, ask them to input another string.
+def problem2():
+    userInput = ""
+    while(userInput != '='):
+        userInput = input("Enter string")
+        userInput2 = input("Enter another string")
+        if userInput == '='
+            print()
+
+#Given non-negative number "num", return True if num is within 2 of a multiple of 10.
+#Print result from function.
+def problem3():
+    num = ""
+    return True
+    if num = with in 2 of multiple of 10
+    print(result)
+    userInput = int(input("Give me a number"))
+def near_ten(num):
+    near_ten(17)--False
+
+#Given array length 1 or more ints, return difference between largest and smallest values in array.
+def problem4():
+    len(arr(int):
+    len(arr(int2)):
+    .difference(largestValues-smallestValues)
+def big_diff(nums):
+    big_diff([2, 10, 7, 2])--8
+
+
